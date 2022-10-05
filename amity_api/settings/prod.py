90eff4419727c84552fa00a60064b5448887f557
annaberezko <site.vizit@gmail.com>
2022-10-05 11:57:09 +0300

    readme changes and structures changes

--- amity_api/settings/prod.py
+++ amity_api/settings/prod.py
@@ -1,7 +1,8 @@
-from decouple import config
-from .base import *
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+from decouple import config
+
+from .base import *
 
 
 DEBUG = config('DEBUG', default=False)
@@ -22,13 +23,6 @@ sentry_sdk.init(
     integrations=[
         DjangoIntegration(),
     ],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
     traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
     send_default_pii=True
 )
