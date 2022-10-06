0ba830a481e0b6a066412fb368f2d0e8f3bff84f
annaberezko <site.vizit@gmail.com>
2022-10-06 14:10:47 +0300

    [SF222-46] change models users, create model manager

--- users/models.py
+++ users/models.py
@@ -1,19 +1,29 @@
 from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser
+    AbstractBaseUser
 )
+from django.core.validators import RegexValidator
 from django.db import models
+
+from .managers import UserManager
 
 
 class User(AbstractBaseUser):
-    id = models.AutoField(primary_key=True)
+    phone_regex = RegexValidator(
+        regex=r"^+?1?\d{10,15}$",
+        message="Phone number must be entered in the format: "
+                "'+999999999'. Up to 15 digits allowed.",
+    )
+
     first_name = models.CharField('first name', max_length=100, null=True, blank=True)
     last_name = models.CharField('last name', max_length=100, null=True, blank=True)
-    email = models.EmailField('email address', unique=True)
-    phone_number = models.CharField('phone number', max_length=20, null=True, blank=True)
+    email = models.EmailField('email address', unique=True, db_index=True)
+    phone_number = models.CharField('phone number', validators=[phone_regex], max_length=20, null=True, blank=True)
     password = models.CharField('password', max_length=100, null=True, blank=True)
     avatar = models.ImageField('user avatar', null=True, blank=True, upload_to='/')
     avatar_coord = models.JSONField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = UserManager()
 
 
 class Profile(models.Model):
@@ -24,13 +34,12 @@ class Profile(models.Model):
     RESIDENT = 5
 
     ROLE_CHOICES = (
-        (AMITY_ADMINISTRATOR, 'Amity administrator'),
+        (AMITY_ADMINISTRATOR, 'amity_administrator'),
         (SUPERVISOR, 'supervisor'),
         (COORDINATOR, 'coordinator'),
         (OBSERVER, 'observer'),
         (RESIDENT, 'resident'),
     )
 
-    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     role = models.SmallIntegerField(choices=ROLE_CHOICES)
