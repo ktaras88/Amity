d635dd62f1f7199462cd5fe96aca3a6d06d2abce
Taras Khaburskyi <ktaras88@gmail.com>
2022-10-13 17:20:52 +0200

    Add tests: create user and superuser and  token pair

--- users/managers.py
+++ users/managers.py
@@ -23,7 +23,7 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email=None, password=None, role=ProfileRoles.RESIDENT, **extra_fields):
         extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
+        # extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, role, True, **extra_fields)
 
     def create_superuser(self, email, password, role=ProfileRoles.AMITY_ADMINISTRATOR, **extra_fields):
