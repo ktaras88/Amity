8b0a4a3e4fee7139b6d86cd102a06fc451a3deda
annaberezko <site.vizit@gmail.com>
2022-10-18 09:22:00 +0300

    [SF222-27] tests changed

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -1,9 +1,11 @@
 from django.urls import reverse
 
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from rest_framework.test import APITestCase, APIClient
 
 from ..models import User, InvitationToken
+from ..serializers import CreateNewPasswordSerializer
 
 
 class TokenObtainPairViewTestCase(APITestCase):
@@ -43,12 +45,27 @@ class ResetPasswordRequestEmailTestCase(APITestCase):
         data = {'email': 'no@no.no'}
         response = self.client.post(self.url,  data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTrue('error' in response.data)
 
     def test_forgot_password_email_in_the_system_check_response(self):
         data = {'email': self.user.email}
         response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(User.objects.get(email=self.user.email).security_code), 6)
+
+    def test_forgot_password_if_request_data_is_empty(self):
+        data = {}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(str(response.data['email'][0]), 'This field is required.')
+
+
+class NewSecurityCodeTestCase(APITestCase):
+    def test_send_security_code_again_code_change(self):
+        user = User.objects.create_user(email='user@user.user')
+        security_code = user.generate_security_code()
+        self.assertEqual(len(security_code), 6)
+        self.assertFalse(user.security_code == security_code)
 
 
 class ResetPasswordSecurityCodeTestCase(APITestCase):
@@ -61,17 +78,25 @@ class ResetPasswordSecurityCodeTestCase(APITestCase):
         data = {'email': 'no@no.no', 'security_code': '000000'}
         response = self.client.post(self.url,  data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTrue('error' in response.data)
 
     def test_security_code_wrong_code(self):
         data = {'email': self.user.email, 'security_code': '000000'}
         response = self.client.post(self.url,  data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTrue('error' in response.data)
 
     def test_security_code_generate_token(self):
         data = {'email': self.user.email, 'security_code': self.user.security_code}
         response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTrue('token' in response.data)
+
+    def test_security_code_empty_data(self):
+        data = {}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(str(response.data['email'][0]), 'This field is required.')
 
 
 class CreateNewPasswordTestCase(APITestCase):
@@ -82,11 +107,36 @@ class CreateNewPasswordTestCase(APITestCase):
         self.token = str(InvitationToken.objects.filter(user=self.user).first())
 
     def test_create_new_password_wrong_token(self):
-        data = {'token': self.token, 'password': 'User-password123', 'confirm_password': 'User-password123'}
+        data = {'token': 'wrong-token', 'password': 'User-password123', 'confirm_password': 'User-password123'}
         response = self.client.post(self.url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        with self.assertRaisesMessage(ValidationError, "There is no access to this page."):
+            serializer = CreateNewPasswordSerializer(data=data)
+            serializer.is_valid(raise_exception=True)
 
     def test_create_new_password_compare_passwords_not_the_same(self):
         data = {'token': self.token, 'password': 'User-password123', 'confirm_password': 'User-password321'}
         response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        with self.assertRaisesMessage(ValidationError, "Passwords do not match"):
+            serializer = CreateNewPasswordSerializer(data=data)
+            serializer.is_valid(raise_exception=True)
+
+    def test_create_new_password_correct(self):
+        data = {'token': self.token, 'password': 'User-password123', 'confirm_password': 'User-password123'}
+        response = self.client.post(self.url, data, format='json')
+        self.assertFalse(self.user.password == data['password'])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_new_password_empty_data(self):
+        data = {}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(str(response.data['token'][0]), 'This field is required.')
+
+    def test_create_new_password_after_successful_request_token_was_removed_from_the_system(self):
+        data = {'token': self.token, 'password': 'User-password123', 'confirm_password': 'User-password123'}
+        response = self.client.post(self.url, data, format='json')
+        self.assertFalse(InvitationToken.objects.filter(user=self.user).count())
+        self.assertFalse(self.user.password == data['password'])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
