ffc4cfe0d2e2a91382a0cfb216f8d716d419944d
annaberezko <site.vizit@gmail.com>
2022-10-19 22:44:56 +0300

    feat/SF-222-82 set up communities

--- amity_api/settings/base.py
+++ amity_api/settings/base.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'communities',
     'users',
     'drf_yasg',
     'rest_framework_simplejwt',
