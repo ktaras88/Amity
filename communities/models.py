19fc6586e88e785cdea716495395ba897e336989
annaberezko <site.vizit@gmail.com>
2022-10-19 22:51:15 +0300

    feat/SF-222-82 set up communities

--- communities/models.py
+++ communities/models.py
@@ -1,8 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.core.validators import RegexValidator
 from django.db import models
+
 from localflavor.us.models import USStateField
 
-from users.models import User
+User = get_user_model()
 
 
 class Community(models.Model):
@@ -20,7 +22,7 @@ class Community(models.Model):
     contact_person = models.ForeignKey(User, on_delete=models.CASCADE, related_name='contact_person', null=True, blank=True)
     phone_number = models.CharField('phone number', validators=[phone_regex], max_length=20)
     description = models.CharField('description', max_length=100, null=True, blank=True)
-    logo = models.ImageField('community logo', null=True, blank=True, upload_to='media/')
+    logo = models.ImageField('community logo', null=True, blank=True, upload_to='media/communities/')
     logo_coord = models.JSONField(null=True, blank=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_by')
-    safety_status = models.BooleanField(default=False)
+    safety_status = models.BooleanField(default=True)
