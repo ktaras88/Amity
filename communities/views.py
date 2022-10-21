3ee8e47d4eecd0e1a25df6d0f7d0dbac7ab62219
annaberezko <site.vizit@gmail.com>
2022-10-21 22:11:54 +0300

    [SF222-58] add page_size_query_param and add check permission when user without token IsOwnerOrReadOnlyNotForResident delete readonly permission

--- communities/views.py
+++ communities/views.py
@@ -1,11 +1,17 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
 
 from amity_api.permission import IsAmityAdministrator
 from .models import Community
 from .serializers import CommunitiesListSerializer
 
 
+class CommunitiesListAPIPagination(PageNumberPagination):
+    page_size_query_param = 'size'
+
+
 class CommunitiesListAPIView(ListAPIView):
     queryset = Community.objects.select_related('contact_person').all()
     serializer_class = CommunitiesListSerializer
     permission_classes = (IsAmityAdministrator, )
+    pagination_class = CommunitiesListAPIPagination
