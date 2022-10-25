b6a5edb219432211ecf7a85d60a7111f2d7387eb
Taras Khaburskyi <ktaras88@gmail.com>
2022-10-25 11:49:37 +0200

    add ListForSearchAPIView

--- communities/urls.py
+++ communities/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import CommunitiesListAPIView
+from .views import CommunitiesListAPIView, ListForSearchAPIView
 
 app_name = 'communities'
 
 urlpatterns = [
     path('', CommunitiesListAPIView.as_view(), name='communities-list'),
+    path('search-list/', ListForSearchAPIView.as_view(), name='search-list'),
 ]
