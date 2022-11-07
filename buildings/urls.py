6935053c3fe0568f243b90ff4701a5d7d8af4e35
TarasKhaburskyi <ktaras88@gmail.com>
2022-11-07 14:21:21 +0100

    [SF222-95] add tests

--- buildings/urls.py
+++ buildings/urls.py
@@ -5,5 +5,5 @@ from .views import BuildingViewSet
 app_name = 'buildings'
 
 urlpatterns = [
-  path('communities/<int:pk>/buildings/', BuildingViewSet.as_view({'post': 'create', 'get': 'list'}), name='buildings'),
+  path('communities/<int:pk>/buildings/', BuildingViewSet.as_view({'post': 'create', 'get': 'list'}), name='buildings-list'),
 ]
